dac74cf6a01edee3379e1e08968d2718cbdf8f9d
Eric Jensen <ericcj@gmail.com>
2018-01-30 15:08:26 -0500

    allow callback to be passed through env if another part of the stack wants to be notified of error

--- lib/catch_json_parse_errors/middleware.rb
+++ lib/catch_json_parse_errors/middleware.rb
@@ -9,6 +9,11 @@ module CatchJsonParseErrors
     rescue ActionDispatch::ParamsParser::ParseError => error
       if env['CONTENT_TYPE'] =~ /application\/json/
         error_output = "There was a problem in the JSON you submitted: #{error.inspect.encode('UTF-8', invalid: :replace, undef: :replace)}"
+
+        if error_callback = env['json_parse_error_callback']
+          error_callback.call(error_output)
+        end
+
         return [
           400, { "Content-Type" => "application/json" },
           [ { status: 400, error: error_output }.to_json ]
