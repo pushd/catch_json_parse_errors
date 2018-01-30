b82648bf56ea17120a0f7b6c8d6fa2ceb407c957
Eric Jensen <ericcj@gmail.com>
2018-01-30 14:59:49 -0500

    inspect

--- lib/catch_json_parse_errors/middleware.rb
+++ lib/catch_json_parse_errors/middleware.rb
@@ -8,7 +8,7 @@ module CatchJsonParseErrors
       @app.call(env)
     rescue ActionDispatch::ParamsParser::ParseError => error
       if env['CONTENT_TYPE'] =~ /application\/json/
-        error_output = "There was a problem in the JSON you submitted: #{error.encode("UTF-8", invalid: :replace, undef: :replace)}"
+        error_output = "There was a problem in the JSON you submitted: #{error.inspect.encode("UTF-8", invalid: :replace, undef: :replace)}"
         return [
           400, { "Content-Type" => "application/json" },
           [ { status: 400, error: error_output }.to_json ]
